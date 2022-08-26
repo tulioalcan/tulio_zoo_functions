5ff0f048fc9f429cbc236b494fcfd9b558aa8901
Túlio Mendes <tulioalcan@gmail.com>
2022-08-26 14:54:02 -0300

    correcao linha requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,6 +2,7 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 const getSpeciesByIds = (...ids) => species.filter((specie) => ids.includes(specie.id));
+// fonte de pesquisa https://desenvolvimentoparaweb.com/javascript/
+// every-some-find-includes-javascript/
 
 module.exports = getSpeciesByIds;
-// fonte de pesquisa https://desenvolvimentoparaweb.com/javascript/every-some-find-includes-javascript/
