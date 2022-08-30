f4d920bf389edda26a003dd69b88ce3cdefd121d
Túlio Mendes <tulioalcan@gmail.com>
2022-08-30 16:09:00 -0300

    requisito 07

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -13,4 +13,16 @@ describe('Testes da função HandlerElephants', () => {
   it('confirmar se o argumento location retornar a localização dos elefantes dentro do Zoológico', () => {
     expect(handlerElephants('location')).toBe('NW');
   });
+  it('confirmar se o argumento popularity retorna a popularidade dos elefantes', () => {
+    expect(handlerElephants('popularity')).toBe(5);
+  });
+  it('confirmar se o argumento availability retornar um array com a relação de dias em que é possível visitar os elefantes', () => {
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
+  it('verifica se handlerElephants é uma função', () => {
+    expect(typeof handlerElephants).toBe('function');
+  });
+  it('confirmar se para nenhum argumento passado a função ela retorna udefined', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
 });
