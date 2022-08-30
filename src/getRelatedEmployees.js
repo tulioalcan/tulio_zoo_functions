4d4b562de7290564cc207d38ea7f6fe34e628816
Túlio Mendes <tulioalcan@gmail.com>
2022-08-30 08:20:49 -0300

    requisito 04

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,19 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  if (isManager(managerId)) {
+    const employeeByManager = data.employees
+      .filter((employee) => employee.managers.includes(managerId));
+    return employeeByManager.map((emp) => `${emp.firstName} ${emp.lastName}`);
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
