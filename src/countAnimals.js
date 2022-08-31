feb9a7a8782421d8bfdacf9f07abf87d4b164d39
Túlio Mendes <tulioalcan@gmail.com>
2022-08-31 15:20:27 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,6 +1,23 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
+  if (animal === undefined) {
+    const totalAnimalforSpecie = species.reduce((acc, { name, residents }) => {
+      acc[name] = residents.length;
+      return acc;
+    }, {});
+    return totalAnimalforSpecie;
+  }
+  const nameSpecie = species.find((specie) => animal.specie === specie.name);
+  const residentsSpecie = nameSpecie.residents;
+  const sumResidentsSpecie = residentsSpecie.length;
+  if (animal.sex && animal.specie) {
+    const sexAnimal = residentsSpecie.filter(({ sex }) => sex === animal.sex);
+    const sumForSex = sexAnimal.length;
+    return sumForSex;
+  }
+  return sumResidentsSpecie;
 }
 
 module.exports = countAnimals;
