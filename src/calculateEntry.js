ec837f5d5553967406089895a06c3b23b6484242
Túlio Mendes <tulioalcan@gmail.com>
2022-08-31 12:29:08 -0300

    requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const ageGroup = {
+    child: entrants.filter((entrant) => entrant.age < 18).length,
+    adult: entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length,
+    senior: entrants.filter((entrant) => entrant.age >= 50).length,
+  };
+  return ageGroup;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || entrants.length === undefined) {
+    return 0;
+  }
+  const childPrice = countEntrants(entrants).child * 20.99;
+  const adultPrice = countEntrants(entrants).adult * 49.99;
+  const seniorPrice = countEntrants(entrants).senior * 24.99;
+  const sum = seniorPrice + adultPrice + childPrice;
+  return sum;
 }
 
 module.exports = { calculateEntry, countEntrants };
